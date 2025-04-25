5556cd1f6581cb002067df04e079550afb6b7abd
David <59452680+davidrnu@users.noreply.github.com>
2025-04-25 11:26:26 +0000

    added id to newObject

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
       const newObject = {
         name: newName,
         number: newNumber,
+        id: persons.length + 1
       };
 
       setPersons(persons.concat(newObject));
