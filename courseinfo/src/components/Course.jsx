8a9009c2446754b66b0c9ece8478831eb856e290
David <59452680+davidrnu@users.noreply.github.com>
2025-04-25 08:39:03 +0000

    courseinfo 2.3 done

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -5,11 +5,17 @@ const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part}/>)
 
+const Total = ({parts}) => {
+    const total = parts.map(part => part.exercises).reduce((total, exercises) => total + exercises)
+    return <b>total of {total} exercises</b>
+}
+
 const Course = ({course}) => {
     return (
         <div>
             <Header course={course}/>
             <Content parts={course.parts}/>
+            <Total parts={course.parts} />
         </div>
     )
 }
