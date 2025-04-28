77014d7ac8cb133d14eab1d3abe9dfcbc6243d2e
David <59452680+davidrnu@users.noreply.github.com>
2025-04-28 08:07:51 +0000

    phonebook 2.12 done

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -31,8 +31,17 @@ const App = () => {
         number: newNumber,
         id: persons.length + 1
       };
+      
+      axios
+        .post('http://localhost:3001/persons', newObject)
+        .then(response => {
+          console.log(response.data)
+          setPersons(persons.concat(response.data))
+          setNewName("")
+          setNewNumber("")
+        })
+      
 
-      setPersons(persons.concat(newObject));
       setNewName("");
     }
   };
